2d5b60a70832821a083087f53880456ffbfc809c
atomita <sleeping.cait.sith+gh@gmail.com>
2016-09-06 20:03:17 +0900

    Create Post.php

---
+++ src/entity/observer/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace atomita\wordpress\eloquent\observer;
+
+class Post
+{
+    function saving($record)
+    {
+        if (! $record->isDirty('post_modified_gmt')) {
+            $record->post_modified_gmt = new Carbon(null, 'GMT');
+        }
+    }
+
+    function creating($record)
+    {
+        if (! $record->isDirty('post_date_gmt')) {
+            $record->post_date_gmt = new Carbon(null, 'GMT');
+        }
+    }
+
+}
